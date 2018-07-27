df430fb83781feec7e132f3955d37cb3fab19807
baru2121 <41731543+baru2121@users.noreply.github.com>
2018-07-27 12:58:27 +0530

    Create year.java

---
+++ year.java
@@ -0,0 +1,36 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		int year;
+		Scanner s=new Scanner(System.in);
+		year=s.nextInt();
+		if((year%400)==0)
+		{
+			System.out.println("yes");
+		}
+		else if((year&100)==0)
+		{
+			System.out.println("no");
+			
+		}
+		else if((year%4)==0)
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		
+		
+		}// your code goes here
+	}
+}
