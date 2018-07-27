6d105827d8ece274e3d1fd3343cf9056089471fd
baru2121 <41731543+baru2121@users.noreply.github.com>
2018-07-27 11:35:39 +0530

    Create consonant.java

---
+++ consonant.java
@@ -0,0 +1,41 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+		
+		class Char
+{
+	public static void main(String[ ] args)
+	{
+	int i=0;
+	Scanner sc=new Scanner(System.in);
+
+	char ch=sc.next( ).charAt(0);	 
+	//char ch=sc.nextChar();
+	switch(ch)
+	{
+	 case  'a' :
+	 case 'e'  :
+	 case 'i'   :
+	 case 'o'  :
+	 case 'u'  :
+	 case  'A' :
+	 case 'E'  :
+	 case 'I'   :
+	 case 'O'  :
+	 case 'U'  :i++;
+	}
+	if(i==1)
+	System.out.println("Vowel");
+	else 
+		if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+		System.out.println(" Consonent");
+		else
+		System.out.println("invalid");		
+	}
+}
+// your code goes here
+	
