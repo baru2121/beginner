f197384160d9560f9f7c5678695b1cba7eee9fa4
baru2121 <41731543+baru2121@users.noreply.github.com>
2018-07-31 14:15:25 +0530

    Update primes.java

--- primes.java
+++ primes.java
@@ -9,15 +9,15 @@ class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-			int n,k;
+	        int n,k;
 		Scanner s=new Scanner(System.in);
 		n=s.nextInt();
 		k=s.nextInt();
 		int m=0;
 		for(int i=n+1;i<k;i++)
 		{
-	int l=0;
-	for(int j=1;j<=i;j++)
+	          int l=0;
+	          for(int j=1;j<=i;j++)
 	{
 		if(i%j==0)
 		{
@@ -25,7 +25,7 @@ class Ideone
 		}
 	}
 	if(l==2)
-		{
+{
 		
 			if(m<l)
 			{
